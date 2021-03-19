a155128aebc4dd718c405fa5798b830684807ac4
datdofpt <dat.doxuan@ggg.com.vn>
2021-03-19 14:01:38 +0700

    '3'

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <Onepie></Onepie>-->
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Onepie></Onepie>
   </div>
 </template>
 
