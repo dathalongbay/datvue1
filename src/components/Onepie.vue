57d10b1cc27a5b6d4a045ed0739633fed83d2200
datdofpt <dat.doxuan@ggg.com.vn>
2021-03-19 13:59:01 +0700

    '2'

---
+++ src/components/Onepie.vue
@@ -0,0 +1,15 @@
+<template>
+  <h2 style="color: orange">Onepie demo components</h2>
+</template>
+
+<script>
+export default {
+  name: 'Onepie',
+  props: {
+  }
+}
+</script>
+
+<style>
+
+</style>
